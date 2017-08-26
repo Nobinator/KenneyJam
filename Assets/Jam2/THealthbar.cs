58fd52df20630ebd668e3e85cba40ce3316c5aa7
Nobinator <nobinator.root@gmail.com>
2017-08-26 21:11:23 +0300

    Healthbar for player

---
+++ Assets/Jam2/THealthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class THealthbar : MonoBehaviour{
+	public Transform target;
+	public Text text;
+
+	public void Set(int h){
+		text.text = new string('-',h);
+	}
+
+	public void SetTarget(Transform t){
+		target = t;
+	}
+
+	// Update is called once per frame
+	void Update (){
+		transform.position = target.position + Vector3.up;
+	}
+}
--- Assets/Jam2/TTank.cs
+++ Assets/Jam2/TTank.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TTank : MonoBehaviour{
 
 	public int armor = 1;
 	public int health = 3;
+	public THealthbar bar;
+
+	private void Start(){
+		bar.Set(health);
+	}
 
 	public bool Damage(int bulletForce){
 		if (bulletForce <= armor) return false;
 		health--;
+		bar.Set(health);
 		return true;
 	}
 
